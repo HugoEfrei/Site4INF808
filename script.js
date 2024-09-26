778eacb00b75a1919f39a742468f3fb68409d9e5
Hugo-transverse <80055149+Hugo-transverse@users.noreply.github.com>
2024-09-26 00:41:00 -0400

    backend

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-var username = document.getElementById("username");
-var pwd = document.getElementById("pwd");
-var btn = document.getElementById("btn");
-var jsonText = document.getElementById("jsontext");
-
-
-btn.addEventListener("click", function(){
-    var data = {
-        "username": username.value,
-        "pwd": pwd.value
-    }
-    jsonText.innerHTML = JSON.stringify(data);
-})
+function SubForm (){
+    $.ajax({
+        url: API_LINK,
+        type:'post',
+        data:$("#myForm").serializeArray(),
+    });
+}
