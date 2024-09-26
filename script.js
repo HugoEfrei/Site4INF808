725a59b7d944e24052ee16f4cf233eb7886a7e3e
Hugo-transverse <80055149+Hugo-transverse@users.noreply.github.com>
2024-09-26 00:44:02 -0400

    alerte for post

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ function SubForm (){
         url: API_LINK,
         type:'post',
         data:$("#myForm").serializeArray(),
+        success: function(){
+            alert("Form Data Submitted :)")
+          },
+          error: function(){
+            alert("There was an error :(")
+          }
     });
 }
