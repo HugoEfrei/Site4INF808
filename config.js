e3d782165d977e2e0b285ede4708d46d172aa421
Hugo-transverse <80055149+Hugo-transverse@users.noreply.github.com>
2024-12-11 19:41:43 -0500

    v1

---
+++ config.js
@@ -0,0 +1,2 @@
+const API_KEY = "https://api.apispreadsheets.com/data/z7jFIdeEkwdCy7dv/";
+export default API_KEY
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function SubForm (){
     $.ajax({
-        url: API_LINK,
+        url:'https://api.apispreadsheets.com/data/z7jFIdeEkwdCy7dv/',
         type:'post',
         data:$("#myForm").serializeArray(),
         success: function(){
-            alert("Form Data Submitted :)")
-          },
-          error: function(){
-            alert("There was an error :(")
-          }
+          alert("Form Data Submitted :)")
+        },
+        error: function(){
+          alert("There was an error :(")
+        }
     });
 }
